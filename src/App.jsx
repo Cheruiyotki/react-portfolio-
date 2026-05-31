2434aea2147405c77912c7783f55bc97e9251721
Cheruiyotki <wilbornkipkoech@gmail.com>
2026-05-31 21:15:36 +0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
- 
+ import {About} from '@/sections/About'
+import {Contact} from '@/sections/Contact'
+import {Experience} from '@/sections/Experience'
+import {Hero} from '@/sections/Hero'
+import {Projects} from '@/sections/Projects'
+import {Testimonals} from '@/sections/Testimonals'
+import { Navbar } from '@/layout/Navbar'
+
+
 function App() {
    
-  return  
-    <>
-       
-    </>
+  return  (
+     <div className="min-h-screen overflow-x-hidden ">
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Projects />
+        <Experience />
+        <Testimonals />
+        <Contact />
+      </main>
+     </div>
+  )
 }
 
 export default App
