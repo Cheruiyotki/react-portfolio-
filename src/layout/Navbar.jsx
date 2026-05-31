8e55081f7b76cfa2b380cc7a94089d2f6b307c80
Cheruiyotki <wilbornkipkoech@gmail.com>
2026-05-31 22:24:10 +0300

    hero section

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import {  Button } from "@/components/Button";
+import { Menu, X } from "lucide-react";
 
 const navLinks = [
     {href: "about", label: "About"},
@@ -8,15 +10,17 @@ const navLinks = [
 ];
 
 export const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
     return (
-    <header className="fixed top-0 right-0 left-0 bg-transparent py-5 ">
+    <header className="fixed top-0 right-0 left-0 bg-transparent py-5 z-50">
         <nav className="container mx-auto px-6 flex items-center justify-between">
             
             <a href="#" className="text-xl font-bold tracking-tight hover:text-primary" >WK
                 <span className="text-primary">.</span></a>
 
             {/* Desktop Nav*/}
-            <div className="flex items-center gap-1 ">
+            <div className="hidden md:flex items-center gap-1 ">
                 <div className="glass rounded-full px-2 py-1 flex items-center gap-2">
                   {navLinks.map((link, index  ) => (
                     <a key={index} href={link.href} className="px-4 py-2 text-sm text-muted-foreground rounded-full hover:text-foreground hover:bg-surface"> {link.label}</a>
@@ -25,13 +29,34 @@ export const Navbar = () => {
             </div>
 
             {/*  CTA button */}
-            <div>
+            <div className="hidden md:block ">
                <Button size="sm">
                 Contact Me
                </Button>
-               
             </div>
+
+
+            {/* Mobile  MEnu button */}
+            <button className="md:hidden  p-2 text-foreground cursor-pointer"
+             onClick={() => setIsMobileMenuOpen((prev) => !prev)} 
+             >
+               {isMobileMenuOpen ? <X size={25}/> : <Menu size={25}/>}
+            </button>
         </nav>
+
+        {/* Mobile Menu */}
+
+        { isMobileMenuOpen && (
+          <div className="md:hidden glass-strong animate-fade-in">
+            <div className="container mx-auto px-6 py-6 flex flex-col gap-4 ">
+              {navLinks.map((link, index  ) => (
+                      <a key={index} href={link.href} className="text-lg text-muted-foregroung hover:text-foreground py-2"> {link.label}</a>
+                    ))}
+                     <Button size="sm">
+                Contact Me
+               </Button>
+          </div>
+        </div>)}
     </header>
     );
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,29 @@
 
 
 export const  Hero = () => {
-    return < section></ section>
+    return (
+    < section className="relative min-h-screen flex items-center overflow-hidden">
+        {/* BG*/}
+        <div className="absolute inset-0 ">
+            <img src="/hero-bg.jpg" alt="Hero Image "
+             className="w-full h-full object-cover opacity-50" />
+             <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background"/> 
+        </div>
+
+        {/* Green Dots */}
+        <div className="absolute inset-0 overflow-hidden pointer-events-none">
+            {[...Array(30)].map((_, i) => (
+                <div className="absolute w-1.5 h-1.5 rounded-full opacity-60"
+                style={{
+                    backgroundColor: "#20B2A6",
+                    left: `${Math.random() * 100}%`,
+                    top: `${Math.random() * 100}%`,
+                    animation:`slow-drift  ${5 + Math.random() * 10}s linear infinite`,
+                    animationDelay: `${Math.random() * 5}s`,
+                }}
+                />
+            ))}
+        </div>
+    </ section>
+    );
 }
