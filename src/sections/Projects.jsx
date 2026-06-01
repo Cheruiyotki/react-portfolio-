3d48f86af1d7ab9565bdef7ccf10995f7cf906c9
Cheruiyotki <wilbornkipkoech@gmail.com>
2026-06-01 23:59:16 +0300

    about section

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -3,10 +3,13 @@ const projects = [
     {
         title: "Hotel Booking App",
         description: "A fullstack hotel booking application built with the PERN stack. It features user authentication, hotel listings, booking management, and a responsive design for seamless user experience across devices.",
-        image: ""
+        image: "/projects/hotelbooking.jpeg",
+        tags: ["React", "Node.js", "Express" ],
+        github: "",
+        link: ""
     },
     {
-        
+
     }
 ]
 
